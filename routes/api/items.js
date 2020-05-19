970a0f726726c84eec36d26d9d444464828ca038
kharioki <antolisah@gmail.com>
2020-05-19 10:34:38 +0300

    added delete item endpoint

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 // @route POST api/items
-// @desc Create a post
+// @desc Create an item
 // @access Public
 router.post('/', (req, res) => {
   const newItem = new Item({
@@ -22,6 +22,15 @@ router.post('/', (req, res) => {
   });
 
   newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items
+// @desc Delete an item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
