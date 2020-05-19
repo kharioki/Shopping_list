37386b1227c2fe78183843b90c1a468f9232275f
kharioki <antolisah@gmail.com>
2020-05-19 09:17:31 +0300

    mongodb config

--- server.js
+++ server.js
@@ -6,3 +6,19 @@ const app = express();
 
 // BodyParser Middleware
 app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
